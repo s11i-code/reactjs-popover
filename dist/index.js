0e4ab5975847b2814394f8e73d26341e8a700102
Satu Salekari <satu.salekari@futurice.com>
2024-11-10 11:42:34 +0200

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testPackage = void 0;
+/* src/index.ts */
+const testPackage = () => {
+    return "Hello World!";
+};
+exports.testPackage = testPackage;
